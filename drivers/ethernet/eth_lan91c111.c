757e8ec9a7d328627a9765b6fc3d4c5e6a275d72
Robin Randhawa <robin.randhawa@arm.com>
2022-03-08 09:56:45 +0000

    WIP commit

--- drivers/ethernet/eth_lan91c111.c
+++ drivers/ethernet/eth_lan91c111.c
@@ -13,39 +13,96 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #include <ethernet/eth_stats.h>
 #include "eth_lan91c111_priv.h"
 
+static uint8_t tx_buffer[ETH_TX_BUF_SIZE];
+
 static void eth_lan91c111_assign_mac(const struct device *dev)
 {
 	uint8_t mac_addr[6] = DT_INST_PROP(0, local_mac_address);
+	uint8_t test_mac_addr[6] = { 0 };
 
 	LOG_INF("%s: %02x:%02x:%02x:%02x:%02x:%02x\n", __FUNCTION__, mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-}
 
-static void eth_lan91c111_flush(const struct device *dev)
-{
-	LOG_INF("%s\n", __FUNCTION__);
-}
+	select_bank(dev, 1);
 
-static void eth_lan91c111_send_byte(const struct device *dev, uint8_t byte)
-{
-	LOG_INF("%s\n", __FUNCTION__);
+	sys_write8(mac_addr[0], MAC_ADDR0);
+	sys_write8(mac_addr[1], MAC_ADDR0 + 1);
+	sys_write8(mac_addr[2], MAC_ADDR1);
+	sys_write8(mac_addr[3], MAC_ADDR1 + 1);
+	sys_write8(mac_addr[4], MAC_ADDR2);
+	sys_write8(mac_addr[5], MAC_ADDR2 + 1);
+
+	select_bank(dev, 1);
+
+	test_mac_addr[0] = sys_read8(MAC_ADDR0);
+	test_mac_addr[1] = sys_read8(MAC_ADDR0 + 1);
+	test_mac_addr[2] = sys_read8(MAC_ADDR1);
+	test_mac_addr[3] = sys_read8(MAC_ADDR1 + 1);
+	test_mac_addr[4] = sys_read8(MAC_ADDR2);
+	test_mac_addr[5] = sys_read8(MAC_ADDR2 + 1);
+
+	LOG_INF("%s: %02x:%02x:%02x:%02x:%02x:%02x\n", __FUNCTION__, test_mac_addr[0], test_mac_addr[1], test_mac_addr[2], test_mac_addr[3], test_mac_addr[4], test_mac_addr[5]);
 }
 
 static int eth_lan91c111_send(const struct device *dev, struct net_pkt *pkt)
 {
+	uint16_t i, data_len, npages, pkt_num, cmd;
+	uint8_t interrupt_status = 0;
+
 	LOG_INF("%s\n", __FUNCTION__);
+
+	data_len = net_pkt_get_len(pkt);
+	npages = ((data_len & ~0x1) + (6 - 1)) >> 8;
+
+	if (npages > 7) {
+		LOG_ERR("%s: TX Packet too big", __FUNCTION__);
+		return -EIO;
+	}
+
+	set_mmu_cmd(dev, npages | MMU_COMMAND_ALLOC);
+
+	for (i = 0; i < 32; i++) {
+		interrupt_status = get_ireg(dev);
+		if (interrupt_status & IMASK_ALLOC_INTR) {
+			set_ireg(dev, IMASK_ALLOC_INTR);
+			break;
+		}
+	}
+
+	if (i == 32) {
+		LOG_ERR("%s: MMU allocation timeout\n", __FUNCTION__);
+		return -EIO;
+	}
+
+	pkt_num = get_ar(dev);
+
+	if (pkt_num & AR_FAILED) {
+		LOG_ERR("%s: AR failed\n", __FUNCTION__);
+		return -EIO;
+	}
+
+	set_pn(dev, pkt_num);
+	set_ptr(dev, PTR_AUTO_INCREMENT);
+
+	set_pkt_header(dev, 0, data_len + 6);
+
+	if (net_pkt_read(pkt, tx_buffer, data_len)) {
+		LOG_ERR("%s: net_pkt_read error\n", __FUNCTION__);
+		return -ENOBUFS;
+	}
+	
+	set_tx_data(dev, tx_buffer, data_len & ~1);
+	
+	if (data_len & 1) {
+		cmd = tx_buffer[data_len - 1] | 0x2000;
+		sys_write8(cmd, DATA_REG);
+		sys_write8(cmd >> 8, DATA_REG + 1);
+	}
+
+	set_mmu_cmd(dev, MMU_COMMAND_ENQUEUE);
+	set_ireg(dev, IMASK_TX_INTR);
+
+	LOG_INF("pkt sent %p len %d", pkt, data_len);
 	return 0;
-}
-
-static void eth_lan91c111_rx_error(struct net_if *iface)
-{
-	LOG_INF("%s\n", __FUNCTION__);
-}
-
-static struct net_pkt *eth_lan91c111_rx_pkt(const struct device *dev,
-					    struct net_if *iface)
-{
-	LOG_INF("%s\n", __FUNCTION__);
-	return NULL;
 }
 
 static void eth_lan91c111_rx(const struct device *dev)
@@ -60,7 +117,25 @@ static void eth_lan91c111_isr(const struct device *dev)
 
 static void eth_lan91c111_init(struct net_if *iface)
 {
+	const struct device *dev = net_if_get_device(iface);
+	const struct eth_lan91c111_config *dev_conf = dev->config;
+	struct eth_lan91c111_runtime *dev_data = dev->data;
+
 	LOG_INF("%s\n", __FUNCTION__);
+
+	dev_data->iface = iface;
+
+	/* Assign link local address. */
+	net_if_set_link_addr(iface,
+			     dev_data->mac_addr, 6, NET_LINK_ETHERNET);
+
+	ethernet_init(iface);
+
+	/* Initialize semaphore. */
+	k_sem_init(&dev_data->tx_sem, 0, 1);
+
+	/* Initialize Interrupts. */
+	dev_conf->config_func(dev);
 }
 
 #if defined(CONFIG_NET_STATISTICS_ETHERNET)
@@ -73,36 +148,6 @@ static struct net_stats_eth *eth_lan91c111_stats(const struct device *dev)
 	return &data->stats;
 }
 #endif
-
-static inline void select_bank(const struct device *dev, uint8_t num)
-{
-	sys_write8(num, REG_BANK_SEL);
-	sys_write8(num >> 8, REG_BANK_SEL + 1);
-}
-
-static inline void set_rcr(const struct device *dev, uint16_t val)
-{
-	select_bank(dev, 0);
-
-	sys_write8(val, REG_RCR);
-	sys_write8(val >> 8, REG_RCR + 1);
-}
-
-static inline void set_tcr(const struct device *dev, uint16_t val)
-{
-	select_bank(dev, 0);
-
-	sys_write8(val, REG_TCR);
-	sys_write8(val >> 8, REG_TCR + 1);
-}
-
-static inline void set_mmu_cmd(const struct device *dev, uint16_t val)
-{
-	select_bank(dev, 2);
-
-	sys_write8(val, REG_MMU_COMMAND);
-	sys_write8(val >> 8, REG_MMU_COMMAND + 1);
-}
 
 static int eth_lan91c111_dev_init(const struct device *dev)
 {
@@ -118,6 +163,13 @@ static int eth_lan91c111_dev_init(const struct device *dev)
 	set_tcr(dev, TCR_CLEAR);
 
 	set_mmu_cmd(dev, MMU_COMMAND_RESET);
+	mmu_busy_wait(dev);
+
+	set_rcr(dev, RCR_ENABLE);
+	set_rcr(dev, TCR_ENABLE);
+
+	set_imask(dev, IMASK_RX_INTR);
+	set_imask(dev, IMASK_TX_INTR);
 
 	return 0;
 }
@@ -125,6 +177,12 @@ static int eth_lan91c111_dev_init(const struct device *dev)
 static void eth_lan91c111_irq_config(const struct device *dev)
 {
 	LOG_INF("%s\n", __FUNCTION__);
+
+	/* Enable Interrupt. */
+	IRQ_CONNECT(DT_INST_IRQN(0),
+		    DT_INST_IRQ(0, priority),
+		    eth_lan91c111_isr, DEVICE_DT_INST_GET(0), 0);
+	irq_enable(DT_INST_IRQN(0));
 }
 
 struct eth_lan91c111_config eth_cfg = {
--- drivers/ethernet/eth_lan91c111_priv.h
+++ drivers/ethernet/eth_lan91c111_priv.h
@@ -1,20 +1,44 @@
 #ifndef ETH_LAN91C111_PRIV_H_
 #define ETH_LAN91C111_PRIV_H_
 
+#define ETH_TX_BUF_SIZE       1536
+
 /*
  *  Register mapping
  */
-#define REG_BANK_SEL        (DEVICE_MMIO_GET(dev) + 0x000e)
-#define REG_RCR             (DEVICE_MMIO_GET(dev) + 0x0004)
-#define REG_TCR             (DEVICE_MMIO_GET(dev) + 0x0000)
-#define REG_MMU_COMMAND     (DEVICE_MMIO_GET(dev) + 0x0000)
+#define BANK_SEL						(DEVICE_MMIO_GET(dev) + 0x000e)
+#define RCR             		(DEVICE_MMIO_GET(dev) + 0x0004)
+#define TCR             		(DEVICE_MMIO_GET(dev) + 0x0000)
+#define MMU_COMMAND     		(DEVICE_MMIO_GET(dev) + 0x0000)
+#define MAC_ADDR0						(DEVICE_MMIO_GET(dev) + 0x0004)
+#define MAC_ADDR1						(DEVICE_MMIO_GET(dev) + 0x0008)
+#define MAC_ADDR2						(DEVICE_MMIO_GET(dev) + 0x000c)
+#define INT_MASK						(DEVICE_MMIO_GET(dev) + 0x000d)
+#define INT_REG				  		(DEVICE_MMIO_GET(dev) + 0x000c)
+#define AR_REG				  		(DEVICE_MMIO_GET(dev) + 0x0003)
+#define PN_REG				  		(DEVICE_MMIO_GET(dev) + 0x0002)
+#define PTR_REG				  		(DEVICE_MMIO_GET(dev) + 0x0006)
+#define DATA_REG				  	(DEVICE_MMIO_GET(dev) + 0x0008)
 
-#define RCR_SOFT_RESET      0x8000
 #define RCR_CLEAR           0x0000
+#define RCR_ENABLE          0x0100
+#define RCR_SOFT_RESET      0x8000
+
 #define TCR_CLEAR           0x0000
+#define TCR_ENABLE          0x0001
+
+#define IMASK_RX_INTR				0x0001
+#define IMASK_TX_INTR				0x0002
+#define IMASK_ALLOC_INTR		0x0008
 
 #define MMU_COMMAND_BUSY    (1 << 0)
+#define MMU_COMMAND_ALLOC   (1 << 5)
 #define MMU_COMMAND_RESET   (2 << 5)
+#define MMU_COMMAND_ENQUEUE (6 << 5)
+
+#define AR_FAILED						0x80
+
+#define PTR_AUTO_INCREMENT	0x4000
 
 struct eth_lan91c111_runtime {
 	DEVICE_MMIO_RAM;
@@ -33,5 +57,141 @@ struct eth_lan91c111_config {
 	DEVICE_MMIO_ROM;
 	eth_lan91c111_config_irq_t config_func;
 };
+
+static inline void select_bank(const struct device *dev, uint8_t num)
+{
+	sys_write8(num, BANK_SEL);
+	sys_write8(num >> 8, BANK_SEL + 1);
+}
+
+static inline void set_rcr(const struct device *dev, uint16_t val)
+{
+	select_bank(dev, 0);
+
+	sys_write8(val, RCR);
+	sys_write8(val >> 8, RCR + 1);
+}
+
+static inline void set_tcr(const struct device *dev, uint16_t val)
+{
+	select_bank(dev, 0);
+
+	sys_write8(val, TCR);
+	sys_write8(val >> 8, TCR + 1);
+}
+
+static inline void set_mmu_cmd(const struct device *dev, uint16_t val)
+{
+	select_bank(dev, 2);
+
+	sys_write8(val, MMU_COMMAND);
+	sys_write8(val >> 8, MMU_COMMAND + 1);
+}
+
+static inline uint16_t get_mmu_cmd(const struct device *dev)
+{
+	uint16_t retval = 0;
+
+	select_bank(dev, 2);
+
+	retval = sys_read8(MMU_COMMAND);
+	retval |= sys_read8(MMU_COMMAND + 1) << 8;
+
+	return retval;
+}
+
+static inline void mmu_busy_wait(const struct device *dev)
+{
+	uint64_t timeout = k_uptime_ticks() + 5;
+
+	while(get_mmu_cmd(dev) & MMU_COMMAND_BUSY) {
+		if (k_uptime_ticks() > timeout) {
+			LOG_ERR("%s: timeout\n", __FUNCTION__);
+			break;
+		}
+		k_cpu_idle();
+	}
+}
+
+static inline void set_imask(const struct device *dev, uint8_t val)
+{
+	select_bank(dev, 2);
+
+	sys_write8(val, INT_MASK);
+}
+
+static inline uint8_t get_imask(const struct device *dev)
+{
+	select_bank(dev, 2);
+
+	return sys_read8(INT_MASK);
+}
+
+static inline void set_ireg(const struct device *dev, uint8_t val)
+{
+	select_bank(dev, 2);
+
+	sys_write8(val, INT_REG);
+}
+
+static inline uint8_t get_ireg(const struct device *dev)
+{
+	select_bank(dev, 2);
+
+	return sys_read8(INT_REG);
+}
+
+static inline uint8_t get_ar(const struct device *dev)
+{
+	select_bank(dev, 2);
+
+	return sys_read8(AR_REG);
+}
+
+static inline uint8_t get_pn(const struct device *dev)
+{
+	select_bank(dev, 2);
+
+	return sys_read8(PN_REG);
+}
+
+static inline void set_pn(const struct device *dev, uint8_t val)
+{
+	select_bank(dev, 2);
+
+	sys_write8(val, PN_REG);
+}
+
+static inline void set_ptr(const struct device *dev, uint16_t val)
+{
+	select_bank(dev, 2);
+
+	sys_write8(val, PTR_REG);
+	sys_write8(val >> 8, PTR_REG + 1);
+}
+
+static inline void set_pkt_header(const struct device *dev, uint16_t status, uint16_t length)
+{
+	select_bank(dev, 2);
+
+	sys_write8(status, DATA_REG);
+	sys_write8(status >> 8, DATA_REG + 1);
+
+	sys_write8(length, DATA_REG);
+	sys_write8(length >> 8, DATA_REG + 1);
+}
+
+static inline void set_tx_data(const struct device *dev, uint8_t *buffer, uint16_t length)
+{
+	uint16_t i, data;
+
+	select_bank(dev, 2);
+
+	for (i = 0; i < length; i++) {
+		data = buffer[i];
+		sys_write8(data, DATA_REG);
+		sys_write8(data >> 8, DATA_REG + 1);
+	}
+}
 
 #endif /* ETH_LAN91C111_PRIV_H_ */
